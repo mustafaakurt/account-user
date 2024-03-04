e29115d9adb7eefd61ccbb4dbbf035ca63ea63e2
Mustafa Kurt <mustafakurt2000@gmail.com>
2024-03-04 17:50:29 +0300

    init project

---
+++ src/test/java/com/mustafaakurt/accountuser/AccountuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustafaakurt.accountuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
